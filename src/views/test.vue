b9dbce47c6cb7776bdca5edd08907d1f4893651c
zhang <zhangqizhen315@gmail.com>
2022-06-12 23:11:38 +0800

    添加了一点演示内容

---
+++ src/views/test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="test">
+    <div class="warpper">
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {reactive} from 'vue'
+// 以上引入模块
+
+
+const myData = reactive({
+  name : '张三',
+  
+})
+console.log(myData);
+
+// 以上定义数据
+
+
+// 以上定义方法
+
+
+</script>
+
+<style scoped lang="less">
+
+</style>
